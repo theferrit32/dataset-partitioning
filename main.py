25f11ffe823c22369567cce30bc589843914188e
Kyle Ferriter <Kyle Ferriter>
2020-05-02 15:26:29 -0400

    add argparser

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import shutil
 import boto3
 import logging
 import pyarrow.parquet as pq
+import argparse
 
 import dataset_partitioning
 from dataset_partitioning.s3.iterator import S3DirectoryIterator
@@ -12,11 +13,17 @@ from dataset_partitioning.partition import ParquetBinPartitioner
 
 
 def main(args):
+    parser = argparse.ArgumentParser(description='dataset-partitioning main')
+    parser.add_argument('s3bucket', type=str)
+    opts = parser.parse_args(args)
+    s3bucket = opts.s3bucket
+    if not s3bucket.startswith('s3://'):
+        s3bucket = 's3://' + s3bucket
+
     session = boto3.Session()
 
     s3 = session.resource('s3')
-    # s3path = 's3://gbsc-aws-project-annohive-dev-user-krferrit-us-west-1/1000Orig-half2-parquet/'
-    s3path = 's3://gbsc-aws-project-annohive-dev-user-krferrit-us-west-1/hg19_Variant_9B_Table_compressed/'
+    s3path = s3bucket + '/hg19_Variant_9B_Table_compressed/'
 
     bin_partitioner = ParquetBinPartitioner(s3, s3path)
     output_path = os.path.join('/mnt/ext', 'partitioned-directory/')
@@ -28,13 +35,6 @@ def main(args):
     # for file_path in S3DirectoryIterator(s3, s3path):
     #     print(file_path)
 
-def main2(args):
-    session = boto3.Session()
-    s3 = session.resource('s3')
-
-    if args[0] == 'getfiles':
-        pass
-
-
 if __name__ == '__main__':
     main(sys.argv[1:])
+
